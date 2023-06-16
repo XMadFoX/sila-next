42e741fed8bd9ab0c6af420d3e7b676e849ff849
MadFox <madfox@laserflare.net>
2023-06-17 03:45:57 +0400

    feat(ui/form): style input field states

--- packages/ui/input/Input.tsx
+++ packages/ui/input/Input.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { ErrorMessage } from '@hookform/error-message';
+import { cva } from 'class-variance-authority';
 import React from 'react';
+import { useController } from 'react-hook-form';
 import { GradientWrapper } from '../general';
+import { cn } from '../lib/utils';
 
 interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	rightItem?: React.ReactNode;
@@ -11,34 +14,65 @@ interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	};
 }
 
+function calcilateVariant({
+	invalid,
+	filled,
+}: {
+	invalid: boolean;
+	filled: boolean;
+}) {
+	if (invalid) return 'invalid';
+	if (filled) return 'filled';
+	return 'default';
+}
+
+const wrapper = cva('flex relative flex-col transition-all duration-300', {
+	variants: {
+		intent: {
+			default: ['bg-primary'],
+			invalid: ['bg-error'],
+			filled: ['bg-black focus-within:bg-primary'],
+		},
+	},
+});
+
 const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
-	({ rightItem, errors, ...props }, ref) => (
-		<div className="flex flex-col">
-			<label className="invisible w-0 h-0" aria-label={props.name}>
-				{props.placeholder}
-			</label>
-			<GradientWrapper
-				className="flex relative flex-col bg-primary"
-				rounded="lg"
-			>
-				<input
-					ref={ref}
-					className="py-2 px-4 w-full rounded-lg ring transition-all duration-300 outline-none focus:ring-transparent invalid:outline-error invalid:border-error invalid:border ring-dark-grey"
-					{...props}
+	({ rightItem, errors, ...props }, ref) => {
+		const { fieldState, field } = useController({ name: props.name! });
+		const variant = calcilateVariant({
+			invalid: fieldState.invalid,
+			filled: field?.value?.length > 0,
+		});
+
+		return (
+			<div className="flex flex-col">
+				<label className="invisible w-0 h-0" aria-label={props.name}>
+					{props.placeholder}
+				</label>
+				<GradientWrapper className={wrapper({ intent: variant })} rounded="lg">
+					<input
+						ref={ref}
+						className={cn(
+							'py-2 px-4 w-full rounded-lg ring transition-all duration-300 outline-none focus:ring-transparent invalid:outline-error invalid:border-error invalid:border ring-dark-grey',
+							field.value?.length > 0 && 'ring-transparent',
+							fieldState.invalid && 'border-error border'
+						)}
+						{...props}
+					/>
+					{rightItem && <div className="absolute right-2">{rightItem}</div>}
+				</GradientWrapper>
+				<ErrorMessage
+					errors={errors}
+					name={props.name!}
+					render={(err) => (
+						<label aria-label="name" className="text-error">
+							{err.message}
+						</label>
+					)}
 				/>
-				{rightItem && <div className="absolute right-2">{rightItem}</div>}
-			</GradientWrapper>
-			<ErrorMessage
-				errors={errors}
-				name={props.name!}
-				render={(err) => (
-					<label aria-label="name" className="text-error">
-						{err.message}
-					</label>
-				)}
-			/>
-		</div>
-	)
+			</div>
+		);
+	}
 );
 
 InputField.displayName = 'InputField';
