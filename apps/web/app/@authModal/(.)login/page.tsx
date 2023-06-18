605a6e8e2eb9dbc284739cf510444230c669b824
MadFox <madfox@laserflare.net>
2023-06-18 22:35:08 +0400

    feat(tailwind): add animations

---
+++ apps/web/app/@authModal/(.)login/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Modal } from 'ui/auth';
+
+export default function page() {
+	return <Modal />;
+}
--- packages/tailwind/tailwind.config.js
+++ packages/tailwind/tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
 				'card-full': '0 0 100%',
 				card: '0 0 200px',
 			},
+			keyframes: {
+				overlayShow: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+				contentShow: {
+					from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+					to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
+				},
+			},
+			animation: {
+				overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+				contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+			},
 		},
 		colors: {
 			transparent: 'transparent',
