847f2add121e4afcb1d099e1fbb599450b35efb1
MadFox <madfox@laserflare.net>
2023-06-28 01:15:07 +0400

    chore: update env example & monor changes

--- packages/api/auth.ts
+++ packages/api/auth.ts
@@ -51,12 +51,8 @@ async function register(credentials: CreateUserInput): Promise<ShortUser> {
 	const verificationToken = crypto.randomBytes(16).toString('hex');
 	// create nodemailer transporter
 	const nodemailer = NodeMailer.createTransport({
-		host: process.env.SMTP_HOST,
-		port: Number(process.env.SMTP_PORT),
-		auth: {
-			user: process.env.SMTP_USER,
-			pass: process.env.SMTP_PASS,
-		},
+		url: process.env.SMTP_URL,
+		from: process.env.SMTP_FROM,
 	});
 
 	const userId = crypto.randomUUID();
