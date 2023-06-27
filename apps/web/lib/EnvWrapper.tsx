3190916ff6995d917757a933a3cc23f0ccf099d4
MadFox <madfox@laserflare.net>
2023-06-27 23:20:51 +0400

    feat(ui): use translation zod errors

--- apps/web/lib/EnvWrapper.tsx
+++ apps/web/lib/EnvWrapper.tsx
@@ -9,6 +9,19 @@ import type { Session } from 'next-auth';
 import { Header } from 'ui';
 import { ErrorBoundary } from 'react-error-boundary';
 
+import i18next from 'i18next';
+import { z } from 'zod';
+import { zodI18nMap } from 'zod-i18n-map';
+import translation from 'zod-i18n-map/locales/ru/zod.json';
+
+i18next.init({
+	lng: 'ru',
+	resources: {
+		ru: { zod: translation },
+	},
+});
+z.setErrorMap(zodI18nMap);
+
 export default function EnvWrapper({
 	children,
 	session,
