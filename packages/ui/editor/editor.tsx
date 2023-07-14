29b717fbfded39801064f00b5d39c88d57d02606
MadFox <madfox@laserflare.net>
2023-07-14 20:18:26 +0400

    fix(editor): creation, add default text

--- packages/ui/editor/editor.tsx
+++ packages/ui/editor/editor.tsx
@@ -20,10 +20,30 @@ export function EditorContainer() {
 						extraDiv?.length === 2 && extraDiv[1].remove();
 						new DragDrop(editor);
 					},
+					data: {
+						blocks: [
+							{
+								id: 'Gjq-FU6zUv',
+								type: 'header',
+								data: {
+									text: 'Заголвок статьи',
+									level: 2,
+								},
+							},
+							{
+								id: 'SGe6wcZQO6',
+								type: 'paragraph',
+								data: {
+									text: 'Текст',
+								},
+							},
+						],
+						version: '2.27.2',
+					},
 				});
 				return editor;
 			}
-			return null;
+			return prevEditor;
 		});
 
 		return () => {
