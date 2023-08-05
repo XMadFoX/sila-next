c64c2337908ad4c6a48950d4bba1e1ea8ddee132
MadFox <madfox@laserflare.net>
2023-08-05 23:56:08 +0400

    fix(events): base content id being null

--- packages/api/schema/contentBase.schema.ts
+++ packages/api/schema/contentBase.schema.ts
@@ -8,18 +8,12 @@ import { users } from './user.schema';
 import { events } from './events.schema';
 import { relations } from 'drizzle-orm';
 
-export const baseContent = sqliteTable(
-	'base_content',
-	{
-		id: integer('id'),
-		title: text('title', { length: 64 }).notNull(),
-		publishedAt: integer('published_at', { mode: 'timestamp_ms' }).notNull(),
-		authorId: text('author_id')
-			.notNull()
-			.references(() => users.id),
-		// organizationId: integer('organization_id').references(() => orgs.id),
-	},
-	(table) => ({
-		pk: primaryKey(table.id),
-	})
-);
+export const baseContent = sqliteTable('base_content', {
+	id: integer('id').primaryKey(),
+	title: text('title', { length: 64 }).notNull(),
+	publishedAt: integer('published_at', { mode: 'timestamp_ms' }).notNull(),
+	authorId: text('author_id')
+		.notNull()
+		.references(() => users.id),
+	// organizationId: integer('organization_id').references(() => orgs.id),
+});
