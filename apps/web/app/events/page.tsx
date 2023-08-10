bdcc3fe3e8d6d0a3bdf27b71a72ea81e21a4bdb1
MadFox <madfox@laserflare.net>
2023-08-10 13:48:47 +0400

    feat(events): add heading to events page

--- apps/web/app/events/page.tsx
+++ apps/web/app/events/page.tsx
@@ -8,28 +8,46 @@ export default async function Events() {
 	// const { data, isFetching } = trpc.events.find.useQuery();
 
 	return (
-		<CardList>
-			{data?.map((i) => {
-				if (!i?.base_content?.title || !i?.users?.name)
-					throw new Error('Not enought data');
+		<section className="mt-16 text-black max-w-[1400px]">
+			<div className="flex">
+				<h1 className="w-1/2 text-2xl font-medium">
+					Раздел направлен
+					<br /> на развитие культурной жизни.
+				</h1>
+				<p className="mt-4 w-1/2 text-lg">
+					Вы можете узнать о культурных мероприятиях, которые проводят члены
+					сообщества онлайн или офлайн, присоединиться к заинтересовавшему вас
+					культурному мероприятию, а также опубликовать информацию о своем
+					мероприятии или проекте.
+				</p>
+			</div>
+			<CardList>
+				{data?.map((i) => {
+					if (!i?.base_content?.title || !i?.users?.name)
+						throw new Error('Not enought data');
 
-				const badges: string[] = [];
-				if (i?.events?.isFree) badges.push('Free');
-				if (i?.events?.isOnline) badges.push('Online');
+					const badges: string[] = [];
+					if (i?.events?.isFree) badges.push('Free');
+					if (i?.events?.isOnline) badges.push('Online');
 
-				return (
-					<Card key={i.events.id}>
-						<Card.Preview image={i.events.coverImage} alt="" badges={badges} />
-						<Card.Details
-							date={i.events.date}
-							title={i.base_content.title}
-							org={{ link: '', name: i.users.name }}
-							location={{ city: 'Aboba' }}
-							description={i.events.description}
-						/>
-					</Card>
-				);
-			})}
-		</CardList>
+					return (
+						<Card key={i.events.id}>
+							<Card.Preview
+								image={i.events.coverImage}
+								alt=""
+								badges={badges}
+							/>
+							<Card.Details
+								date={i.events.date}
+								title={i.base_content.title}
+								org={{ link: '', name: i.users.name }}
+								location={{ city: 'Aboba' }}
+								description={i.events.description}
+							/>
+						</Card>
+					);
+				})}
+			</CardList>
+		</section>
 	);
 }
