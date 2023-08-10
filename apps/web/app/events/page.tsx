4573d45d8c841d48dca63babf292da4a15dd54ca
MadFox <madfox@laserflare.net>
2023-08-10 14:52:10 +0400

    feat(events/page): add important events block

--- apps/web/app/events/page.tsx
+++ apps/web/app/events/page.tsx
@@ -1,7 +1,7 @@
 import { getEvents } from '@sila/api';
 import CardList from 'components/landing/CardsContainer';
 import React from 'react';
-import { Card } from 'ui';
+import { Card, Heading, Slide, Slider } from 'ui';
 
 export default async function Events() {
 	const data = await getEvents();
@@ -20,6 +20,33 @@ export default async function Events() {
 					мероприятии или проекте.
 				</p>
 			</div>
+			<section>
+				<Heading>Важные события</Heading>
+				<div className="flex mx-auto max-w-fit">
+					<Slider className="mx-auto mt-8">
+						{data?.map((i, idx) => (
+							<Slide key={i.events.id}>
+								<Card big key={i.events.id} gradientClass="w-full min-w-max">
+									<Card.Preview
+										image={i.events.coverImage}
+										alt=""
+										className="max-w-min"
+										big
+										badges={['Free', 'Online']}
+									/>
+									<Card.Details
+										date={i.events.date}
+										title={i.base_content.title}
+										org={{ link: '', name: i.users.name }}
+										location={{ city: 'Aboba' }}
+										description={i.events.description}
+									/>
+								</Card>
+							</Slide>
+						))}
+					</Slider>
+				</div>
+			</section>
 			<CardList>
 				{data?.map((i) => {
 					const badges: string[] = [];
