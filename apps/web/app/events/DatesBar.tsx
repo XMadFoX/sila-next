b7e22e3849eed392559e3e32e237ee5fe5d14412
MadFox <madfox@laserflare.net>
2023-08-11 19:46:53 +0400

    feat(events): active state for day in dates bar

--- apps/web/app/events/DatesBar.tsx
+++ apps/web/app/events/DatesBar.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import clsx from 'clsx';
+import { cn } from 'ui/lib/utils';
 import React from 'react';
 import { Button } from 'ui';
 import { $selectedDate } from './date.atom';
+import { useStore } from '@nanostores/react';
 
 function getNextDays(numDays: number): number[] {
 	const today = new Date();
@@ -26,21 +27,29 @@ export default function DatesBar() {
 	const dayDigitFormat = new Intl.DateTimeFormat('ru-RU', { day: '2-digit' });
 	const dayFormat = new Intl.DateTimeFormat('ru-RU', { weekday: 'short' });
 
+	const selectedDate = useStore($selectedDate);
+
 	return (
 		<div className="mt-8">
 			<ul className="flex overflow-x-scroll gap-2 pb-4 w-full">
 				{dates.map((date, idx) => (
 					<li key={`${idx}:${date}`}>
 						<Button
-							onClick={() => $selectedDate.set(new Date(date))}
+							onClick={() => {
+								console.log(new Date(date));
+								console.log(selectedDate);
+								$selectedDate.set(new Date(date));
+							}}
 							size={null}
 							aria-label={`Выбрать дату ${date} ${new Intl.DateTimeFormat(
 								'ru-RU',
 								{ month: 'long' }
 							).format(new Date())}`}
-							className={clsx(
-								'text-xl font-medium',
-								'flex flex-col p-2 justify-center items-center w-8 h-12 from-10% to-90% bg-center rounded-lg hover:text-white hover:bg-gradient-to-br from-primary-a to-primary-c'
+							className={cn(
+								'text-xl font-medium relative',
+								'flex flex-col p-2 justify-center items-center w-8 h-12 from-10% to-90% bg-center rounded-lg hover:text-white hover:bg-gradient-to-br from-primary-a to-primary-c',
+								selectedDate.getTime() === date &&
+									'text-white bg-gradient-to-br bg-primary'
 							)}
 							intent="clear"
 						>
