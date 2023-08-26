c68402178fa8d75c3ec024e19a6f9fd074e06d25
MadFox <madfox@laserflare.net>
2023-08-27 03:36:58 +0400

    feat(client): disable retries on 404

--- apps/web/lib/TrpcProvider.tsx
+++ apps/web/lib/TrpcProvider.tsx
@@ -1,14 +1,29 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
+import { TRPCClientError, httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
 import { trpc } from './trpc';
 import superjson from 'superjson';
 import { env } from '@sila/api/env.mjs';
 
 export const TrpcProvider: React.FC<{ children: React.ReactNode }> = (p) => {
-	const [queryClient] = useState(() => new QueryClient());
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						retry: (failureCount, error) => {
+							if (error instanceof TRPCClientError) {
+								if (error.data.httpStatus === 404) return false;
+							}
+							if (failureCount > 3) return false;
+							return true;
+						},
+					},
+				},
+			})
+	);
 	const [trpcClient] = useState(() =>
 		trpc.createClient({
 			transformer: superjson,
