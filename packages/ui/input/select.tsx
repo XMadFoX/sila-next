2f4b4c7e03414c9062af5fe1dc456b166a0ff087
MadFox <madfox@laserflare.net>
2023-09-14 20:10:20 +0400

    fix(ui/input): select backround color

--- packages/ui/input/select.tsx
+++ packages/ui/input/select.tsx
@@ -26,7 +26,7 @@ const SelectTrigger = React.forwardRef<
 	>
 		{children}
 		<SelectPrimitive.Icon asChild>
-			<ChevronDown className="h-4 w-4 opacity-50" />
+			<ChevronDown className="w-4 h-4 opacity-50" />
 		</SelectPrimitive.Icon>
 	</SelectPrimitive.Trigger>
 ));
@@ -86,9 +86,9 @@ const SelectItem = React.forwardRef<
 		)}
 		{...props}
 	>
-		<span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+		<span className="flex absolute left-2 justify-center items-center w-3.5 h-3.5">
 			<SelectPrimitive.ItemIndicator>
-				<Check className="h-4 w-4" />
+				<Check className="w-4 h-4" />
 			</SelectPrimitive.ItemIndicator>
 		</span>
 
