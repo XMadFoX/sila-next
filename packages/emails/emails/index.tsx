93db1641bd1971b1a204ba7e5d0fe2ac054eac93
MadFox <madfox@laserflare.net>
2023-10-20 02:40:19 +0400

    Revert "chore(emails): update example"
    This reverts commit f605b092cb7f73512a6113fbf06de0899e014eb3.

--- packages/emails/emails/index.tsx
+++ packages/emails/emails/index.tsx
@@ -3,25 +3,28 @@ import { Html } from '@react-email/html';
 import { Tailwind } from '@react-email/tailwind';
 import * as React from 'react';
 import config from 'tailwind-config';
-
-const tailwindConfig = {
-	...config,
-	theme: {
-		extend: {},
-	},
-};
+// import 'web/app/global.css';
+import './global.css';
 
 export default function Email() {
 	return (
-		<Tailwind config={tailwindConfig as any}>
-			<Html>
+		<Html>
+			<Tailwind config={config as any}>
+				{/* <style> */}
+				{/* 	:root { */}
+				{/* 		--error: #cb1212; */}
+				{/* }</style> */}
+				{/* {JSON.stringify(config.theme)} */}
 				<Button
-					href="https://google.com"
-					className="p-4 text-white bg-blue-900"
+					pX={20}
+					pY={12}
+					href="https://example.com"
+					style={{ background: 'red', color: '#fff' }}
 				>
-					Google
+					Click me
 				</Button>
-			</Html>
-		</Tailwind>
+				<Button className="p-4 bg-green-500 bg-error">tailwind</Button>
+			</Tailwind>
+		</Html>
 	);
 }
