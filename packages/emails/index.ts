98fe0a391520e8c282da7b0b8a96299c5dbf07ea
MadFox <madfox@laserflare.net>
2023-10-22 22:36:05 +0400

    feat(emails): add published (approved ad) template

--- packages/emails/index.ts
+++ packages/emails/index.ts
@@ -3,3 +3,4 @@ export { Template as NewLoginTemplate } from './templates/NewLogIn';
 export { Template as TotpStatusChanged } from './templates/TotpStatusChanged';
 export { Template as ModRequest } from './templates/ModRequest';
 export { Template as ChangesRequested } from './templates/ChangesRequested';
+export { Template as PublishedTemplate } from './templates/Published';
---
+++ packages/emails/templates/Published.tsx
@@ -0,0 +1,30 @@
+import { Heading, Text } from '@jsx-email/all';
+import { Template as MainTemplate } from './MainTemplate';
+
+export const TemplateName = 'Published';
+
+export const Template = ({
+	timestamp,
+	title,
+	url,
+}: {
+	timestamp: string;
+	title: string;
+	message: string;
+	username: string;
+	url: string;
+}) => {
+	return (
+		<MainTemplate
+			title={`Опубликовано объявление "${title}"`}
+			text=""
+			actionText="Открыть"
+			actionUrl={url}
+			body={
+				<>
+					<Text className="text-center">{timestamp} по UTC</Text>
+				</>
+			}
+		/>
+	);
+};
