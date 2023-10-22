c3b03cafbaccf91e91375ad66be70d6968c53d1e
MadFox <madfox@laserflare.net>
2023-10-22 22:35:44 +0400

    feat(emails): add changes requested template

---
+++ packages/emails/templates/ChangesRequested.tsx
@@ -0,0 +1,32 @@
+import { Heading, Text } from '@jsx-email/all';
+import { Template as MainTemplate } from './MainTemplate';
+
+export const TemplateName = 'ChangesRequested';
+
+export const Template = ({
+	timestamp,
+	title,
+	message,
+	url,
+}: {
+	timestamp: string;
+	title: string;
+	message: string;
+	url: string;
+}) => {
+	return (
+		<MainTemplate
+			title={`Запрошены изменения для "${title}"`}
+			text={`Запрос на изменения "${title}"`}
+			actionText="Открыть"
+			actionUrl={url}
+			body={
+				<>
+					<Heading>Сообщение</Heading>
+					<Text className="text-start">{message}</Text>
+					<Text className="text-center">{timestamp} по UTC</Text>
+				</>
+			}
+		/>
+	);
+};
