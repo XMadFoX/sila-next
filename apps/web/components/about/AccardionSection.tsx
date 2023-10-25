cbfbf6bf8e7863908dcb97370f7349a3c80b2577
MadFox <madfox@laserflare.net>
2023-10-26 00:46:37 +0400

    refactor(web/about): decompose section

--- apps/web/components/about/AccardionSection.tsx
+++ apps/web/components/about/AccardionSection.tsx
@@ -6,31 +6,33 @@ import {
 } from 'ui/@/components/ui/accordion';
 import { faqItems, ruleItems } from './items';
 
+const sections = [
+	{ title: 'Часто задаваемые вопросы', items: faqItems },
+	{ title: 'Правила для участников проекта', items: ruleItems },
+];
+
 export default function AccardionSection() {
 	return (
 		<section className="mt-10 lg:max-w-5xl xl:mt-24">
-			<h2 className="text-xl font-medium xl:text-3xl">
-				Часто задаваемые вопросы
-			</h2>
-			<Accordion type="single" collapsible className="w-full">
-				{faqItems.map((item) => (
-					<AccordionItem key={item.value} value={item.value}>
-						<AccordionTrigger>{item.triggerText}</AccordionTrigger>
-						<AccordionContent>{item.content}</AccordionContent>
-					</AccordionItem>
-				))}
-			</Accordion>
-			<h2 className="mt-10 text-xl font-medium xl:text-3xl">
-				Правила для участников проекта
-			</h2>
-			<Accordion type="single" collapsible className="w-full">
-				{ruleItems.map((item) => (
-					<AccordionItem key={item.value} value={item.value}>
-						<AccordionTrigger>{item.triggerText}</AccordionTrigger>
-						<AccordionContent>{item.content}</AccordionContent>
-					</AccordionItem>
-				))}
-			</Accordion>
+			{sections.map((section) => (
+				<Section key={section.title} {...section} />
+			))}
 		</section>
+	);
+}
+
+function Section({ title, items }: { title: string; items: typeof faqItems }) {
+	return (
+		<>
+			<h2 className="mt-10 text-xl font-medium xl:text-3xl">{title}</h2>
+			<Accordion type="single" collapsible className="w-full">
+				{items.map((item) => (
+					<AccordionItem key={item.value} value={item.value}>
+						<AccordionTrigger>{item.triggerText}</AccordionTrigger>
+						<AccordionContent>{item.content}</AccordionContent>
+					</AccordionItem>
+				))}
+			</Accordion>
+		</>
 	);
 }
