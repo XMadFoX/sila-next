b8e3f6c49ec8e628faffcbc838510d32c37480c6
MadFox <madfox@laserflare.net>
2023-10-27 19:23:11 +0400

    fix(web/about): text color

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -3,7 +3,7 @@ import AccardionSection from 'components/about/AccardionSection';
 
 export default function Page() {
 	return (
-		<main className=" w-full max-w-[1400px] mt-10 px-4 2xl:px-0">
+		<main className=" w-full max-w-[1400px] mt-10 px-4 2xl:px-0 text-black">
 			<AboutSection />
 			<AccardionSection />
 		</main>
